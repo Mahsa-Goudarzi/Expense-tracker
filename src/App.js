48c9ce67d758150340ccaa4d9e1ca52117629c54
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-01-10 22:41:58 +0330

    added a new component for expenses

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
     <div className="App">
       <h1>Let's get started!</h1>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ExpenseItem() {
+  return <div className="ExpenseItem">hello from exchange item</div>;
+}
