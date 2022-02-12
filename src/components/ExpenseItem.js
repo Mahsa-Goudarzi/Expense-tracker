9aeeb9aa7787707ad8ed85f0a28f0dc31f75179b
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-02-12 11:53:26 +0330

    Add ExpenseItem.css file and add classes to ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 export default function ExpenseItem() {
   return (
     <div className="ExpenseItem">
       <div>Date</div>
-      <div>
+      <div className="expense-item-description">
         <h2>Title</h2>
-        <div>Amount</div>
+        <div className="expense-item-price">Amount</div>
       </div>
     </div>
   );
