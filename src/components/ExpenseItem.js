a074a9053fce74f8b7221057ac6e6a2ab8f2964b
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-02-12 11:28:11 +0330

    Add placeholders for title, amount and date in Expense component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function ExpenseItem() {
-  return <div className="ExpenseItem">hello from exchange item</div>;
+  return (
+    <div className="ExpenseItem">
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+        <div>Amount</div>
+      </div>
+    </div>
+  );
 }
