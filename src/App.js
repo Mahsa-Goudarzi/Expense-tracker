627d09aae64b36ff55576223db24512207a468f0
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-02-16 22:02:36 +0330

    pass props to ExpenseItem component to make it dynamic

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { id: "e1", title: "title 1", amount: 12.31, date: new Date(2020, 7, 14) },
+    { id: "e2", title: "title 2", amount: 125.2, date: new Date(2021, 7, 14) },
+    { id: "e3", title: "title 3", amount: 1.35, date: new Date(2020, 8, 5) },
+    { id: "e4", title: "title 4", amount: 300.56, date: new Date(2019, 1, 29) },
+  ];
+
   return (
     <div className="App">
       <h1>Let's get started!</h1>
-      <ExpenseItem />
+      <ExpenseItem expense={expenses[0]} />
+      <ExpenseItem expense={expenses[1]} />
+      <ExpenseItem expense={expenses[2]} />
+      <ExpenseItem expense={expenses[3]} />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
+  let expense = props.expense;
+  let date = expense.date.toISOString();
+  let title = expense.title;
+  let amount = expense.amount;
+
   return (
     <div className="ExpenseItem">
-      <div>Date</div>
+      <div>{date}</div>
       <div className="expense-item-description">
-        <h2>Title</h2>
-        <div className="expense-item-price">Amount</div>
+        <h2>{title}</h2>
+        <div className="expense-item-price">{amount}</div>
       </div>
     </div>
   );
