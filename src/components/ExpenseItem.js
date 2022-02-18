1c7305e302d3777b3bb6a5e1f62783721792351f
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-02-18 20:26:23 +0330

    Show the date in a month/year/day format in ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,13 +3,20 @@ import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
   let expense = props.expense;
-  let date = expense.date.toISOString();
+  let date = expense.date;
+  let month = date.toLocaleString("en-US", { month: "long" });
+  let day = date.toLocaleString("en-US", { day: "2-digit" });
+  let year = date.getFullYear();
   let title = expense.title;
   let amount = expense.amount;
 
   return (
     <div className="ExpenseItem">
-      <div>{date}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item-description">
         <h2>{title}</h2>
         <div className="expense-item-price">{amount}</div>
