abc7fdeb143ff8acc55811efdd3f57a65d4d0492
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-02-25 23:13:20 +0330

    add Card component to Expenses component to wrap it

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import Card from "./Card";
 
 export default function Expenses(props) {
   let expenses = props.expenses;
   return (
-    <div className="Expenses">
+    <Card className="Expenses">
       <ExpenseItem expense={expenses[0]} />
       <ExpenseItem expense={expenses[1]} />
       <ExpenseItem expense={expenses[2]} />
       <ExpenseItem expense={expenses[3]} />
-    </div>
+    </Card>
   );
 }
