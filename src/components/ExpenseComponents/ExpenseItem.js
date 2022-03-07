8cacf2805e47897af631c1b1de27fc444877f8fa
Mahsa Goudarzi <mahsagoodarzi1999@gmail.com>
2022-03-07 22:17:28 +0330

    organize files

--- src/components/ExpenseComponents/ExpenseItem.js
+++ src/components/ExpenseComponents/ExpenseItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 export default function ExpenseItem(props) {
   let expense = props.expense;
@@ -14,7 +14,7 @@ export default function ExpenseItem(props) {
       <ExpenseDate date={date} />
       <div className="expense-item-description">
         <h2>{title}</h2>
-        <div className="expense-item-price">{amount}</div>
+        <div className="expense-item-price">${amount}</div>
       </div>
     </Card>
   );
--- src/components/ExpenseComponents/Expenses.js
+++ src/components/ExpenseComponents/Expenses.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 export default function Expenses(props) {
   let expenses = props.expenses;
